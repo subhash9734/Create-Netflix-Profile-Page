fb996ac030bc61ae6aefa5f235db460c07b0208e
subhash9734 <120626096+subhash9734@users.noreply.github.com>
2023-07-22 12:22:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const users = ['Krishna', 'Radha', 'Subhash'];
+const memberDiv = document.querySelector('.memberDiv');
+
+const addIcon = document.querySelector('.addIcon');
+
+const userIcons = () => {
+    users.reverse();
+    users.map((curElem) => {
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn"><span>${curElem}</span></button>
+        `);
+    })
+};
+
+addIcon.addEventListener('click', () => {
+    let userName = prompt('please enter your name')
+
+    if (userName != null & !users.includes(userName)) {
+        users.push(userName);
+        console.log(users);
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn"><span>${userName}</span></button>
+        `);
+    } else {
+        alert('username alredy exist');
+    }
+})
+
+userIcons();
